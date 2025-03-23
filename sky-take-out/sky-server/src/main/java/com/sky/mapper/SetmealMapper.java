c8623a2a22986d5b7ce8156d69c59f2b9e1ecf1d
“cedar” <1991863702@qq.com>
2025-03-23 18:56:37 +0800

    【新功能】分页查询套餐

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,10 +1,15 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -30,4 +35,15 @@ public interface SetmealMapper {
      */
     @AutoFill(value = OperationType.INSERT)
     void insert(Setmeal setmeal);
+
+    /**
+     * 分页查询套餐
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pagequery(SetmealPageQueryDTO setmealPageQueryDTO);
+
+
+
+
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -1,11 +1,16 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
+import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,5 +43,19 @@ public class SetmealServiceImpl implements SetmealService {
             setmealDishMapper.insert(setmealDish);
         }
 
+    }
+
+    /**
+     * 分页查询套餐
+     *
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    public PageResult pagequery(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageHelper.startPage(setmealPageQueryDTO.getPage(), setmealPageQueryDTO.getPageSize());
+        Page<SetmealVO> page = setmealMapper.pagequery(setmealPageQueryDTO);
+        Long total = page.getTotal();
+        List<SetmealVO> setmealVOList = page.getResult();
+        return new PageResult(total, setmealVOList);
     }
 }
